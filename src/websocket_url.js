269c98e77f3fe95769b06fdd9b55952394106e98
Keenan Brock <keenan@thebrocks.net>
2015-09-20 13:57:59 -0400

    merge a.href = a.href into previous line

--- src/websocket_url.js
+++ src/websocket_url.js
@@ -1,8 +1,7 @@
 var websocketUrl = function (url) {
   'use strict';
   var a =  document.createElement('a');
-  a.href = url;
-  a.href = a.href; // IE bug (maybe not necessary)
+  a.href = a.href = url;
   a.protocol = a.protocol.replace("http", "ws");
   return a.href;
 };
