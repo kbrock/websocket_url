7de640b89d3dd3fa3f1ff9d0c74b8f9e2739934b
Keenan Brock <keenan@thebrocks.net>
2015-09-20 22:12:22 -0400

    Put in test dir (for relative url) symlink src
    i.e. doesn't handle ../src too well, so put link into test dir

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,7 +36,7 @@ module.exports = function(grunt) {
     'saucelabs-jasmine': {
       all: {
         options: {
-          urls: ["http://127.0.0.1:9999/SpecRunner.html"],
+          urls: ["http://127.0.0.1:9999/test/SpecRunner.html"],
           tunnelTimeout: 5,
           build: process.env.TRAVIS_JOB_ID,
           concurrency: 3,
