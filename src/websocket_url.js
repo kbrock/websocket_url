489d09c9841adbc0cfec652a43b2660fd1b95995
Keenan Brock <keenan@thebrocks.net>
2015-09-20 23:44:44 -0400

    fix a.href for internet explorer with comment

--- src/websocket_url.js
+++ src/websocket_url.js
@@ -1,7 +1,9 @@
 var websocketUrl = function (url) {
   'use strict';
   var a =  document.createElement('a');
-  a.href = a.href = url;
+  a.href = url;
+  /* ie fix for relative urls */
+  a.href = a.href;
   a.protocol = a.protocol.replace("http", "ws");
   return a.href;
 };
