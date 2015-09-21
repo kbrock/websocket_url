5514951962b07f47d6b4f38253ad444c3cf1b45a
Keenan Brock <keenan@thebrocks.net>
2015-09-20 22:19:55 -0400

    fix safari websocket detection and remove "should"

--- src/websocket_url.spec.js
+++ src/websocket_url.spec.js
@@ -3,26 +3,26 @@
 describe("WebsocketUrl", function() {
   'use strict';
   // we only care about browsers if there is web socket support
-  it("should Support websockets", function() {
-    expect(typeof(WebSocket)).toEqual("function");
+  it("supports websockets", function() {
+    expect(typeof(WebSocket)).not.toEqual("undefined");
   });
-  it("should understand absolute http urls", function() {
+  it("understands absolute http urls", function() {
     expect(websocketUrl("http://example.com/sub/")).toEqual("ws://example.com/sub/");
   });
-  it("should understand absolute https urls", function() {
+  it("understands absolute https urls", function() {
     expect(websocketUrl("https://example.com/sub/")).toEqual("wss://example.com/sub/");
   });
-  it("should understand absolute urls", function() {
+  it("understands absolute urls", function() {
     expect(websocketUrl("/funny")).toMatch(/^wss?:\/\/[^\/]*\/funny$/);
   });
-  it("should understand relative urls", function() {
+  it("understands relative urls", function() {
     // NOTE: relative url derived from test suite named: "test/SpecRunner.html"
     expect(websocketUrl("funny")).toMatch(/^wss?:\/\/[^\/]*\.*\/test\/funny$/);
   });
-  it("should understand root urls", function() {
+  it("understands root urls", function() {
     expect(websocketUrl("/")).toMatch(/^wss?:\/\/[^\/]*\.*\/$/);
   });
-  it("should understand blank urls", function() {
+  it("understands blank urls", function() {
     expect(websocketUrl("")).toMatch(/^wss?:\/\/[^\/]*\.*\/test\/SpecRunner.html$/);
   });
 });
